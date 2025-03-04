96a84101d769189ae6fadfecdefec7c9b69f224f
A2ureeE <568806893@qq.com>
2025-03-04 16:29:55 +0800

    Update0.1

---
+++ pythonpart/main.py
@@ -0,0 +1,20 @@
+# 这是一个示例 Python 脚本。
+
+# 按 Ctrl+F5 执行或将其替换为您的代码。
+# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+
+def print_hi(name):
+    # 在下面的代码行中使用断点来调试脚本。
+    print(f'Hi, {name}')  # 按 F9 切换断点。
+#输出一个数字1
+    print(1)
+#输出python版本号
+    print("python版本号")
+    import sys
+    print(sys.version)
+# 按装订区域中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
---
+++ test.cpp
@@ -0,0 +1,11 @@
+//输出一个字母1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "cnmgit" << endl;
+    return 0;
+}
+    
